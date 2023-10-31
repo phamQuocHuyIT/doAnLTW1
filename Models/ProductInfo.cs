376228022979670930930bda81b068c5c2b988ea
phamQuocHuyIT <huycttv2203@gmail.com>
2023-10-31 15:31:13 +0700

    ProductInfo is Model

---
+++ Models/ProductInfo.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doAnLTW01.Models
+{
+    public class ProductInfo
+    {
+        public int proId {  get; set; }
+        public string proName { get; set; }
+        public float proPrice { get; set; }
+        public string cardID { get; set; }
+        public string style { get; set; }
+    }
+}
